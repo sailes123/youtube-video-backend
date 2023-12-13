946d33c48f52f99a1f15824ef69bb5bf56630fa4
Sailes123 <saileshbajgain056@gmail.com>
2023-12-13 11:41:46 +0545

    controller added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,8 @@
 class ApiError extends Error {
         constructor(statusCode,
                 message = "Something went wrong",
-                error= [],
-                statck= ""
+                errors= [],
+                stack= ""
                 ){
                 super(message)
                 this.statusCode = statusCode
