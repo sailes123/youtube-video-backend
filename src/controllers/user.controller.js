ba64465f386b5f0bb9ebe6da5092a7c8320e9465
Sailes123 <saileshbajgain056@gmail.com>
2023-12-13 12:55:15 +0545

    logic build

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,35 @@
-import asyncHandler from '../utils/asyncHandlers.js'
+import asyncHandler from "../utils/asyncHandlers.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
-const registerUser = asyncHandler( async (req, res) => {
-        res.status(200).json({
-                message: "ok"
-        })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exist: username, email
+  // check for images, check for avatar
+  // upload them from cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
 
-export {registerUser};
+  const { fullName, email, username, password } = req.body; // you will get direct data from body if data is coming from form or json
+
+  if (
+    [fullName, email, username, password].some((field) => {
+      field?.trim() === "";
+    })
+  ) {
+    throw new ApiError(400, "This field is required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if(existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+  }
+});
+
+export { registerUser };
